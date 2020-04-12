3cc8dc235aae3d3a23d9ef64dfe7e515666b3082
tw <dongnae.kang@gmail.com>
2020-04-12 20:46:55 +0900

    Grunt

--- js/script.js
+++ js/script.js
@@ -12,7 +12,9 @@ $(document).ready(function() {
             $("#carouselButton").children("span").addClass('fa-pause');                    
         }
     });
+});
 
+$(document).ready(function() {
     $("#loginButton").click(function(){
         $('#loginModal').modal('toggle');
     })
